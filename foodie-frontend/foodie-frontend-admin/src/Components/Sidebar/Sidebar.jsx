fdd89d213575b2ef76924f848d5e427eb6f3f4c9
LwinMoeAung <lwinmoeaung.it@gmail.com>
2025-03-08 00:44:04 +0900

    remove promo code

--- foodie-frontend/foodie-frontend-admin/src/Components/Sidebar/Sidebar.jsx
+++ foodie-frontend/foodie-frontend-admin/src/Components/Sidebar/Sidebar.jsx
@@ -7,29 +7,29 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-            <NavLink to='/dashboard' className="sidebar-option">
-                <img src={assets.order_icon} alt="add icon" />
-                <p>Dashboard</p>
-            </NavLink>
-            <NavLink to='/add' className="sidebar-option">
-                <img src={assets.add_icon} alt="add icon" />
-                <p>Add Item</p>
-            </NavLink>
-            <NavLink to='/list' className="sidebar-option">
-                <img src={assets.order_icon} alt="add icon" />
-                <p>List Item</p>
-            </NavLink>
-            <NavLink to='/order' className="sidebar-option">
-                <img src={assets.order_icon} alt="add icon" />
-                <p>Orders</p>
-            </NavLink>
-            <NavLink to='/promocode' className="sidebar-option">
+        <NavLink to="/dashboard" className="sidebar-option">
+          <img src={assets.order_icon} alt="add icon" />
+          <p>Dashboard</p>
+        </NavLink>
+        <NavLink to="/add" className="sidebar-option">
+          <img src={assets.add_icon} alt="add icon" />
+          <p>Add Item</p>
+        </NavLink>
+        <NavLink to="/list" className="sidebar-option">
+          <img src={assets.order_icon} alt="add icon" />
+          <p>List Item</p>
+        </NavLink>
+        <NavLink to="/order" className="sidebar-option">
+          <img src={assets.order_icon} alt="add icon" />
+          <p>Orders</p>
+        </NavLink>
+        {/* <NavLink to='/promocode' className="sidebar-option">
                 <img src={assets.order_icon} alt="add icon" />
                 <p>Promo Code</p>
-            </NavLink>
+            </NavLink> */}
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
